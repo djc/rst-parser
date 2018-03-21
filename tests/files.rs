5825861929ef99481b843cf470d0482bb6292158
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-03-21 14:10:13 +0100

    Add initial integration test

---
+++ tests/files.rs
@@ -0,0 +1,19 @@
+extern crate rst_parser;
+
+use rst_parser::{writers, parser};
+use rst_parser::writers::Writer;
+
+macro_rules! check_files {
+    ($in_fn:expr, $out_fn:expr) => (
+        let input = include_str!($in_fn);
+        let output = include_str!($out_fn);
+        let ast = parser::document(input.as_bytes()).unwrap().1;
+        let html = writers::Html5Writer::translate(ast);
+        assert_eq!(html, output);
+    )
+}
+
+#[test]
+fn test_paragraphs() {
+    check_files!("paragraphs.rst", "paragraphs.html");
+}
